58728c3dc8075878b63ada8bdcff8270a99c6462
Kate <katekrupnik@gmail.com>
2023-01-12 13:30:19 +0300

    begin cod for ex02

---
+++ ex02/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//Принять число Number от пользователя
+// Найти summ цифер в числе
+
+int GetNumberByUser(string Number)
+{
+  Console.WriteLine("Введите число: ");
+  string Number = Convert.ToInt32(Console.ReadLine());
+}
+int Number = GetNumberByUser();
